e16df7301868528a7de15801d4479ef00b286dee
Alex Waite <alexandrawaite@gmail.com>
2020-04-18 10:55:48 -0700

    Codesandbox test

--- src/index.js
+++ src/index.js
@@ -2,24 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-  return (
+const Square = (props) => (
     <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   );
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a]
+    }
+  }
+  return null;
 }
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
+  state = {
+    squares: Array(9).fill(null),
+    xIsNext: true,
+  };
 
-  handleClick(i) {
+  handleClick = (i) => {
     const squares = this.state.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
@@ -31,7 +47,7 @@ class Board extends React.Component {
     });
   }
 
-  renderSquare(i) {
+  renderSquare = (i) => {
     return (
       <Square
         value={this.state.squares[i]}
@@ -86,27 +102,6 @@ class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
-  }
-  return null;
 }
 
 // ========================================
